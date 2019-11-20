b2529d143ffbb97d11a1cfa02ecdd0e75f483c6d
luxin <luxinmu@163.com>
2019-11-20 21:41:56 +0800

    sync home test.

--- src/test/Tools.java
+++ src/test/Tools.java
@@ -3,9 +3,16 @@ package test;
 public class Tools {
 
 	public static void printArray(int[] nums) {
-		for (int i = 0; i < nums.length; i++) {
+		for (int i = 0; i < nums.length; i++)
 			System.out.print(nums[i] + (i == nums.length - 1 ? "" : ","));
-		}
 		System.out.println();
+	}
+
+	public static void printArray(int[][] nums) {
+		for (int nums0[] : nums) {
+			for (int i = 0; i < nums0.length; i++)
+				System.out.print(nums0[i] + (i == nums0.length - 1 ? "" : ","));
+			System.out.println();
+		}
 	}
 }
