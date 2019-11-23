cc72c6349ad68b8977dd4d1393291bb638d03ebe
twobugs <luxinmu@aliyun.com>
2019-11-23 12:26:19 +0800

    test61 end.

---
+++ src/test/T61_RotateList.java
@@ -0,0 +1,38 @@
+package test;
+
+public class T61_RotateList {
+    public static void main(String[] args) {
+        T61_RotateList t61 = new T61_RotateList();
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        ListNode.print(n1);
+        ListNode.print(t61.rotateRight(n1, 2));
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null)
+            return head;
+        int count = 0;
+        ListNode tail = head;
+        while (tail.next != null) {
+            count++;
+            tail = tail.next;
+        }
+        tail.next = head;  //KEYPOINT make circle
+        count++;
+        ListNode prevbreak = head;
+        int loop = count - k % count - 1;
+        while (loop-- > 0)
+            prevbreak = prevbreak.next;
+        head = prevbreak.next;  //set new head
+        prevbreak.next = null;  //cut circle
+        return head;
+    }
+}
