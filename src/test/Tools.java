db4e5d88439073b3c0bbf4cbb9bf6a721f7ced64
twobugs <luxinmu@aliyun.com>
2019-11-25 09:31:26 +0800

    tools update.

--- src/test/Tools.java
+++ src/test/Tools.java
@@ -2,6 +2,14 @@ package test;
 
 public class Tools {
 
+	public static void printArray(char[] nums) {
+		if (nums == null || nums.length == 0)
+			return;
+		for (int i = 0; i < nums.length; i++)
+			System.out.print(nums[i] + (i == nums.length - 1 ? "" : ","));
+		System.out.println();
+	}
+
 	public static void printArray(int[] nums) {
 		if (nums == null || nums.length == 0)
 			return;
